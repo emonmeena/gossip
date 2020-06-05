b34a01a3e7d5cd72dd3eba50ac7e3aa813571f29
mayank-genesis <mayank16meena@gmail.com>
2020-06-05 12:23:49 +0530

    Add Notification sound

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -7,15 +7,15 @@ const users = {};
 io.on('connection', socket=>{
     socket.on('new-user-joined', name =>{
         users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
-    });
+        socket.broadcast.emit('user-joined', name)
+    })
 
     socket.on('send', message=>{
         socket.broadcast.emit('recieve', {message: message, name: users[socket.id]})
-    });
+    })
     socket.on('disconnect', message=>{
         socket.broadcast.emit('leave', users[socket.id])
         delete users[socket.id]
-    });
+    })
 
 })
