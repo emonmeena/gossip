120efc711297db44bced60100de821c8a4b79603
mayank-genesis <mayank16meena@gmail.com>
2020-06-06 13:49:21 +0530

    Add online activity section

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -7,14 +7,24 @@ const users = {};
 io.on('connection', socket=>{
     socket.on('new-user-joined', name =>{
         users[socket.id] = name;
+        const newNameId = socket.id;
         socket.broadcast.emit('user-joined', name)
+        socket.broadcast.emit('user-show', name)    
+        socket.broadcast.emit('other-users-show', newNameId)
+    })
+
+    socket.on('show-existing-users', newNameId =>{
+        console.log(users[socket.id], newNameId)
+        socket.broadcast.to(newNameId).emit('user-show', users[socket.id])
     })
 
     socket.on('send', message=>{
         socket.broadcast.emit('recieve', {message: message, name: users[socket.id]})
     })
+ 
     socket.on('disconnect', message=>{
         socket.broadcast.emit('leave', users[socket.id])
+        socket.broadcast.emit('user-offline', users[socket.id])
         delete users[socket.id]
     })
 
--- client.js
+++ client.js
@@ -3,10 +3,12 @@ const socket = io('http://localhost:8000');
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInput');
 const messageContainer = document.querySelector(".container");
+const userContainer = document.querySelector(".container2")
 
 const sendTune = new Audio('send.mp3');
 const recieveTune = new Audio('recieve.mp3');
 
+// JS function to inform other users about recent connection
 const append = (message, position)=>{
 const messageElement = document.createElement('div');
 messageElement.innerHTML = message;
@@ -15,10 +17,11 @@ messageElement.classList.add(position);
 messageContainer.append(messageElement);
 if(position == 'left'){
     recieveTune.play();
-}
+    }
 else sendTune.play();
 }
 
+//  when we click send button this function sends mssgs
 form.addEventListener('submit', (e)=>{
     e.preventDefault(); //prevent reloading
     const message = messageInput.value; 
@@ -27,15 +30,47 @@ form.addEventListener('submit', (e)=>{
     messageInput.value = ""
 })
 
+// JS function to add user in the "Online-list"
+const appendUser = (name)=>{
+    const userElement = document.createElement('div');
+    userElement.classList.add('user');
+    userElement.innerHTML = name;
+    userElement.id = name;
+    userContainer.append(userElement);
+    }
+
+const appendExistingUser=(newNameId)=>{
+    socket.emit('show-existing-users', newNameId)
+}
+
+const removeUser = (name)=>{
+    const user = document.getElementById(name)
+    user.remove()
+}
+
+    // Client Interface
 const name = prompt("Enter your name to Join the Room: ");
-socket.emit('new-user-joined', name);
+document.getElementById('profile').innerHTML = "Your Public Name: "+name;
+socket.emit('new-user-joined', name); //sends name to the server
 
 socket.on('user-joined',name=>{
  append(`${name} joined the Room`, 'right');
 })
 
+socket.on('user-show', name=>{
+    appendUser(name);
+})
+
+socket.on('other-users-show', newNameId=>{
+    appendExistingUser(newNameId)
+})
+
 socket.on('recieve', data =>{
     append(`${data.name}: ${data.message}`, 'left')
+})
+
+socket.on('user-offline', name=>{
+    removeUser(name)
 })
 
 socket.on('leave', name =>{
